2a5bb963a8d78d44b85935e94a545d880a95305d
yousefdewidar <ymahmoud1213@gmail.com>
2025-04-14 14:24:06 +0200

    create exam ui part1.

--- src/app/question/create-new-exam/create-new-exam.component.ts
+++ src/app/question/create-new-exam/create-new-exam.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-create-new-exam',
-  imports: [RouterLink],
   templateUrl: './create-new-exam.component.html',
-  styleUrl: './create-new-exam.component.css'
+  styleUrls: ['./create-new-exam.component.css'],
+  imports: [CommonModule, DragDropModule],
+  standalone: true
 })
 export class CreateNewExamComponent {
+  currentStep: number = 1;
+
+  questions = [
+    { title: 'رأس السؤال الأول', type: 'Multiple Choice', points: 30 },
+    { title: 'رأس السؤال الثاني', type: 'Multiple Choice', points: 10 },
+  ];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
+  }
+
+  nextStep() {
+    this.currentStep++;
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) this.currentStep--;
+  }
 
 }
